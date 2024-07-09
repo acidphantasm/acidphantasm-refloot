bb751229462905a252a10289c9d1f66e67a0950a
acidphantasm <127812106+acidphantasm@users.noreply.github.com>
2024-07-09 04:52:04 -0500

    final commit

--- src/mod.ts
+++ src/mod.ts
@@ -1,64 +1,88 @@
 import { DependencyContainer } from "tsyringe";
 
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
-import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { DatabaseService } from "@spt/services/DatabaseService";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
-import { ItemHelper } from "@spt/helpers/ItemHelper";
-import { BaseClasses } from "@spt/models/enums/BaseClasses";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
 
-class Mod implements IPostDBLoadMod
+class BossesHaveLegaMedals implements IPostDBLoadMod
 {
+    private logger: ILogger
+
     public postDBLoad(container: DependencyContainer): void
     {
-        // get database from server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const databaseService = container.resolve<DatabaseService>("DatabaseService");
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+        const tables: IDatabaseTables = databaseService.getTables();
 
-        // Get all the in-memory json found in /assets/database
-        const tables: IDatabaseTables = databaseServer.getTables();
+        const glukhar = tables.bots.types.bossgluhar.inventory.items.Pockets;
+        const kaban = tables.bots.types.bossboar.inventory.items.Pockets;
+        const killa = tables.bots.types.bosskilla.inventory.items.Pockets;
+        const kollontay = tables.bots.types.bosskolontay.inventory.items.Pockets;
+        const reshala = tables.bots.types.bossbully.inventory.items.Pockets;
+        const sanitar = tables.bots.types.bosssanitar.inventory.items.Pockets;
+        const shturman = tables.bots.types.bosskojaniy.inventory.items.Pockets;
+        const tagilla = tables.bots.types.bosstagilla.inventory.items.Pockets;
 
-        // ---------------------------------------------------------
-        // example #1
-        // Make the LEDX item sellable on flea market
+        glukhar["6656560053eaaa7a23349c86"] = 2800;
+        kaban["6656560053eaaa7a23349c86"] = 6750;
+        killa["6656560053eaaa7a23349c86"] = 7000;
+        kollontay["6656560053eaaa7a23349c86"] = 5950;
+        reshala["6656560053eaaa7a23349c86"] = 4420;
+        sanitar["6656560053eaaa7a23349c86"] = 4550;
+        shturman["6656560053eaaa7a23349c86"] = 4300;
+        tagilla["6656560053eaaa7a23349c86"] = 500;
 
-        // Find the ledx item by its Id
-        const ledx = tables.templates.items["5c0530ee86f774697952d952"];
+        /*
+        THIS IS ALL STUPID BUT I AM TIRED WHICH MAKES ME STUPID
+        If you want to have something to get probability with weights, please do it better than me. LOL.
 
-        // Update one of its properties to be true
-        ledx._props.CanSellOnRagfair = true;
+        const glukharTotal = Object.values(glukhar).reduce((a, b) => a + b, 0)
+        const kabanTotal = Object.values(kaban).reduce((a, b) => a + b, 0)
+        const killaTotal = Object.values(killa).reduce((a, b) => a + b, 0)
+        const kollontayTotal = Object.values(kollontay).reduce((a, b) => a + b, 0)
+        const reshalaTotal = Object.values(reshala).reduce((a, b) => a + b, 0)
+        const sanitarTotal = Object.values(sanitar).reduce((a, b) => a + b, 0)
+        const shturmanTotal = Object.values(shturman).reduce((a, b) => a + b, 0)
+        const tagillaTotal = Object.values(tagilla).reduce((a, b) => a + b, 0)
 
-        // ---------------------------------------------------------
-        // example #2
-        // Get globals settings and set flea market min level to be 1
-        tables.globals.config.RagFair.minUserLevel = 1;
+        let value;
+        let count;
 
-        // ---------------------------------------------------------
-        // Example #3
-        // Loop over all magazines and make them weigh nothing
+        value = 2800;
+        count = Object.keys(glukhar).length;
+        this.logger.log(`glukhar: ${glukharTotal} --- if value: ${value} then chance is ${value / (glukharTotal+value)} -- total items: ${count}`, "yellow");
 
-        // Get ItemHelper ready to use
-        const itemHelper: ItemHelper = container.resolve<ItemHelper>("ItemHelper");
+        value = 6750;
+        count = Object.keys(kaban).length;
+        this.logger.log(`kaban: ${kabanTotal} --- if value: ${value} then chance is ${value / (kabanTotal+value)} -- total items: ${count}`, "yellow");
 
-        // Get all items in the database as an array so we can loop over them later
-        // tables.templates.items is a dictionary, the key being the items template id, the value being the objects data,
-        // we want to convert it into an array so we can loop over all the items easily
-        // Object.values lets us grab the 'value' part as an array and ignore the 'key' part
-        const items = Object.values(tables.templates.items);
+        value = 7000;
+        count = Object.keys(killa).length;
+        this.logger.log(`killa: ${killaTotal} --- if value: ${value} then chance is ${value / (killaTotal+value)} -- total items: ${count}`, "yellow");
 
-        // Use the itemHelper class to assist us in getting only magazines
-        // We are filtering all items to only those with a base class of MAGAZINE (5448bc234bdc2d3c308b4569)
-        const magazines = items.filter(x => itemHelper.isOfBaseclass(x._id, BaseClasses.MAGAZINE));
+        value = 5950;
+        count = Object.keys(kollontay).length;
+        this.logger.log(`kollontay: ${kollontayTotal} --- if value: ${value} then chance is ${value / (kollontayTotal+value)} -- total items: ${count}`, "yellow");
 
-        // Loop over all the magazines the above code found
-        for (const magazine of magazines)
-        {
-            // Check the magazine has a weight property before we edit it
-            if (magazine._props.Weight)
-            {
-                // Set its weight to 0
-                magazine._props.Weight = 0;
-            }
-        }
+        value = 4420;
+        count = Object.keys(reshala).length;
+        this.logger.log(`reshala: ${reshalaTotal} --- if value: ${value} then chance is ${value / (reshalaTotal+value)} -- total items: ${count}`, "yellow");
+
+        value = 4550;
+        count = Object.keys(sanitar).length;
+        this.logger.log(`sanitar: ${sanitarTotal} --- if value: ${value} then chance is ${value / (sanitarTotal+value)} -- total items: ${count}`, "yellow");
+
+        value = 4300;
+        count = Object.keys(shturman).length;
+        this.logger.log(`shturman: ${shturmanTotal} --- if value: ${value} then chance is ${value / (shturmanTotal+value)} -- total items: ${count}`, "yellow");
+
+        value = 500;
+        count = Object.keys(tagilla).length;
+        this.logger.log(`tagilla: ${tagillaTotal} --- if value: ${value} then chance is ${value / (tagillaTotal+value)} -- total items: ${count}`, "yellow");
+        
+        */
     }
 }
 
-export const mod = new Mod();
+export const mod = new BossesHaveLegaMedals();
